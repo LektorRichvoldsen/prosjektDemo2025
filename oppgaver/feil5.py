5c0bcfd11ffb96931e4656bfd557b109c16294ee
LektorRichvoldsen <erria002@osloskolen.no>
2025-12-10 08:36:31 +0100

    La til nye oppgaver

---
+++ oppgaver/feil5.py
@@ -0,0 +1,5 @@
+else: 
+print ("du er ikke myndig ennå")
+print ("du er over 18")
+if alder > 18:
+alder = 19
---
+++ oppgaver/feil6.py
@@ -0,0 +1,7 @@
+poenggrense = 20
+_____ = int(input("Skriv inn hvor mange poeng du fikk: "))
+ 
+if ______ >= ______:
+    print("Du har bestått! :)")
+_______:
+    print("Du har ikke bestått. :(")
